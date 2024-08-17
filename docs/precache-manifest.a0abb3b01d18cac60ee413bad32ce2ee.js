5933a40be5cfca63f499ca3b1c568321a3c527fc
Issei Sakamoto <banyen@MacBookPro.local>
2024-08-17 18:36:33 +0900

    first commit

---
+++ docs/precache-manifest.a0abb3b01d18cac60ee413bad32ce2ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e91f3a8b93727f09ab",
+    "url": "css/app.d0cc553f.css"
+  },
+  {
+    "revision": "fbc8b8f58ef9710ff46c",
+    "url": "css/chunk-vendors.8884985b.css"
+  },
+  {
+    "revision": "42cc00481867d612e4b3398017500ac6",
+    "url": "img/books.42cc0048.png"
+  },
+  {
+    "revision": "65875f6894e8796f72efd0e758787e34",
+    "url": "img/shopping.65875f68.png"
+  },
+  {
+    "revision": "bde8b70f18f5b07103dffb94509ec2c9",
+    "url": "index.html"
+  },
+  {
+    "revision": "e7e91f3a8b93727f09ab",
+    "url": "js/app.8b2d09ce.js"
+  },
+  {
+    "revision": "fbc8b8f58ef9710ff46c",
+    "url": "js/chunk-vendors.c7e8b383.js"
+  },
+  {
+    "revision": "c0e3f939cb1addfe524aa5429a87efe7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
